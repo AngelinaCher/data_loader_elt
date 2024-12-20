795efd50b21f038e00a59af773dd19dd3a12e606
angelina <cher.rly2036@gmail.com>
2024-12-20 10:43:05 +0300

    chore: инициализация проекта

---
+++ src/config/config.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+env_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_ECHO = True
